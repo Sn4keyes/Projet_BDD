bc301cca50413ea9e78bbb3fa14b0272e877bb07
Defaillances <mathieu.son@live.fr>
2022-01-20 17:52:01 +0100

    ELECTRICITY DATA

--- electricity_producer.py
+++ electricity_producer.py
@@ -3,6 +3,7 @@
 from pycoingecko import CoinGeckoAPI
 from kafka import KafkaProducer
 import numpy as np
+import requests
 import json
 import time
 import sys
@@ -10,46 +11,20 @@ import sys
 BROKER = 'localhost:9092'
 TOPIC = 'electricity0'
 
-def clean_json(json, crypto_name):
-    json_clean = {
-        crypto_name + "_timestamp": [],
-        crypto_name + "_prices": [],
-        crypto_name + "_market_cap": [],
-        crypto_name + "_total_vol": []
-    }
-    js_line = 0
-    for js_col in json:
-        middle_js_line = len(json[js_col][js_line]) // 2
-        for i in json[js_col]:
-            if js_line <= 168:
-                if json[js_col] == json["prices"]:
-                    json_clean[crypto_name + "_timestamp"].extend(json[js_col][js_line][:middle_js_line])
-                    json_clean[crypto_name + "_prices"].extend(json[js_col][js_line][middle_js_line:])
-                elif json[js_col] == json["market_caps"]:
-                    json_clean[crypto_name + "_market_cap"].extend(json[js_col][js_line][middle_js_line:])
-                elif json[js_col] == json["total_volumes"]:
-                    json_clean[crypto_name + "_total_vol"].extend(json[js_col][js_line][middle_js_line:])
-                js_line += 1
-        js_line = 0
-    return json_clean
+list_code = ["DE","FR","CA-ON"]
 
-def producer_ethereum():
-    ethereum = cg.get_coin_market_chart_by_id(
-                id="ethereum",
-                vs_currency='usd',
-                include_market_cap=True,
-                days='7'
-            )
-    clean_ethereum = clean_json(ethereum, "ETH")
-    return clean_ethereum
+def call_electricity_api():
+    myToken = 'JBolGXLpHpqhxpjeRbVKUt7onZB8bpMS'
+    head = {'auth-token': myToken}
+    df = []
+    for i in list_code:
+        time.sleep(2)
+        myUrl = 'https://api.co2signal.com/v1/latest?countryCode=%s' % (i)
+        response = requests.get(myUrl, headers=head)
+        electricity_data = response.json()
+        df.append(electricity_data)
+    return df
 
-def call_crypto_api():
-    json_full = {}
-    
-    bitcoin = producer_ethereum()
-    json_full.update(bitcoin)
-
-    return json_full
 
 if __name__ == "__main__":
     
@@ -62,7 +37,7 @@ if __name__ == "__main__":
     
     # while True:
     print("########## Send Data To Kafka: OK ##########")
-    json_full = call_crypto_api()
-    producer.send(TOPIC, json.dumps(json_full).encode('utf-8'))
+    electricity_json = call_electricity_api()
+    producer.send(TOPIC, json.dumps(electricity_json).encode('utf-8'))
     producer.flush()
     # sleep(5)
--- work/electricity_consumer.py
+++ work/electricity_consumer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from logging import root
 from pyspark.sql.types import StructType, StructField, FloatType, IntegerType, StringType
 from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession
@@ -15,48 +16,72 @@ import time
 BROKER = 'kafka:9093'
 TOPIC = 'electricity0'
 
-def post_in_bdd(spark_df):
-    spark_df.write.format("mongo").mode("append").option("database","electricity").option("collection", "electricity_col").save()
+# def post_in_bdd(spark_df):
+#     spark_df.write.format("mongo").mode("append").option("database","electricity").option("collection", "electricity_col").save()
 
-def spark_connect():
-    spark = SparkSession    \
-            .builder    \
-            .master('local')    \
-            .appName('Crypto')  \
-            .config("spark.mongodb.input.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin")  \
-            .config("spark.mongodb.output.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin") \
-            .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0") \
-            .config("spark-jars-packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1")  \
-            .getOrCreate()
-    spark.sparkContext
-    return spark
+# def spark_connect():
+#     spark = SparkSession    \
+#             .builder    \
+#             .master('local')    \
+#             .appName('Crypto')  \
+#             .config("spark.mongodb.input.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin")  \
+#             .config("spark.mongodb.output.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin") \
+#             .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0") \
+#             .config("spark-jars-packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1")  \
+#             .getOrCreate()
+#     spark.sparkContext
+#     return spark
+
+# mongo_client = MongoClient('localhost', 27017)
+# db = mongo_client.electricity_data
+# col = db.electricity_col
+# username = 'root'
+# password = 'root'
+
+
+def post_in_bdd(electricity_col, df):
+    data = df.to_dict()
+    electricity_col.insert_one(data)
+
+def _connect_mongo(host, port, username, password, db):
+
+    if username and password:
+        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+
+    return conn[electricity_col]
+
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+
+    db = _connect_mongo(host=host, port=port, username=root, password=password, db=db)
+
+    cursor = db[collection].find(query)
+
+    df =  pd.DataFrame(list(cursor))
+
+    if no_id:
+        del df['_id']
+
+    return df
 
 if __name__ == "__main__":
 
     try:
         client = MongoClient('mongo', 27017, username = 'root', password = 'root')
-        db_crypto = client.crypto
-        crypto_col = db_crypto.crypto_col
-        calcul_col = db_crypto.calcul_col
-        crypto_col.drop()
-        calcul_col.drop()
+        db_electricity = client.electricity
+        electricity_col = db_electricity.electricity_col
+        electricity_col.drop()
         print("########## Création de la base de données ##########")
     except:
         print("Erreur de connexion à MongoDB")
     consumer = KafkaConsumer(TOPIC, bootstrap_servers=[BROKER], api_version=(2,6,0))
-    spark = spark_connect()
+
     for msg in consumer:
         print("########## Received Data From Producer : OK ##########")
         df = pd.DataFrame.from_dict(json.loads(msg.value))
         print("\n########## DataFrame Pandas : ##########\n")
         print(df)
-        print("\n########## Minimum : ##########")
-        print(df["ETH_prices"].min())
-        print("\n########## Maximum : ##########")
-        print(df["ETH_prices"].max())
-        print("\n########## Average : ##########")
-        print(df["ETH_prices"].mean())
-        spark_df = spark.createDataFrame(df)
-        post_in_bdd(spark_df)
-        print("\n########## DataFrame Spark : ##########\n")
-        spark_df.show(5, False)
+        post_in_bdd(electricity_col, df)
